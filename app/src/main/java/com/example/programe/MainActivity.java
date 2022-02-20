3160f42f67bb1d8062772394f6505f6e6719dd64
andreyskakunenko <RhfvfnjhcR>
2022-02-20 02:07:15 +0200

    fix navigation add user icon in bottom navigation

--- app/src/main/java/com/example/programe/MainActivity.java
+++ app/src/main/java/com/example/programe/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_add_book, R.id.navigation_reading_now)
+                R.id.navigation_home, R.id.navigation_add_book, R.id.navigation_reading_now,R.id.navigation_user)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
